bdb074cdaabaf846b536a180fbb3f8bbdcc6d038
Fabiano Franz <ffranz@redhat.com>
2011-11-03 13:46:38 -0200

    Improved long polling client

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,5 +1,23 @@
 markers = new Array();
+
+(function poll(){
+    $.ajax({ 
+        url: "../async", 
+        dataType: "json", 
+        async: true, 
+        cache: false, 
+        timeout: 30000,
+//        complete: poll, 
+        success: function(data) {
+            handleData(data);
+        } 
+    });
+})();
 	
+    handleData = function(data) {
+            alert(data);
+    }
+	                   
 $(document).ready(function() {
                     
     var position = new google.maps.LatLng(37.774947, -122.419414);
@@ -13,47 +31,8 @@ $(document).ready(function() {
         });
 
     var infoWindow = new google.maps.InfoWindow({});
+    
+    poll();
 
-    $.stream("../async", {
-        type: "http",
-        dataType: "json",
-        context: $("#map")[0],
-        open: function(event, stream) {
-            stream.send({});
-        },
-        message: function(event) {
-            $.each(event.data, function(index1, update) { 
-                if (update.object == "geography") {
-                    $.getJSON("../details?object=" + update.object_id, function(instagrams) {
-                        $.each(instagrams.data, function(index2, instagram) {
-                            var key = instagram.user.username + ':' + instagram.location.latitude + ':' + instagram.location.longitude;
-                            if ($.inArray(key, markers) == -1) {
-                                markers.push(key);
-                                var p = new google.maps.LatLng(instagram.location.latitude, instagram.location.longitude);
-                                var marker = new google.maps.Marker({
-                                    position: p,
-                                    title: instagram.user.username
-                                });
-                                marker.setMap(map);
-                                infoWindow.setContent(tmpl("info", { 
-                                    username: instagram.user.username, 
-                                    width: instagram.images.thumbnail.width, 
-                                    height: instagram.images.thumbnail.height, 
-                                    thumbnail: instagram.images.thumbnail.url }));
-                                infoWindow.open(map, marker);
-                            }
-
-                        });
-                    });
-                }
-            });
-        },
-        error: function() {
-            alert("error");
-        },
-        close: function() {
-        }
-    });
-	                   
 });
 
