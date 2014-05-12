bceb4df7d72532c5afd63422e65140c3223e886f
fabianofranz <contact@fabianofranz.com>
2014-05-12 15:36:44 -0300

    Testing

--- Server.java
+++ Server.java
@@ -7,17 +7,30 @@ import java.util.Map;
 public class Server extends Verticle {
  
   public void start() {
+
     int port = Integer.parseInt(System.getenv("OPENSHIFT_VERTX_PORT"));
     String ip = System.getenv("OPENSHIFT_VERTX_IP");
+
     vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
       public void handle(HttpServerRequest req) {
+
         System.out.println("Got request: " + req.uri());
         System.out.println("Headers are: ");
+
         for (Map.Entry<String, String> entry : req.headers()) {
           System.out.println(entry.getKey() + ":" + entry.getValue());
         }
+
         req.response().headers().set("Content-Type", "text/html; charset=UTF-8");
-        req.response().end("<html><body><h1>Hello from Java on vert.x!</h1></body></html>");
+
+	if (request.path().equals("/hello")) {
+          request.response().end("Hello from Java !!!");
+
+        } else {
+          String file = request.path().equals("/") ? "index.html" : request.path();
+          request.response().sendFile("webroot/" + file);
+        }
+
       }
     }).listen(port, ip);
   }
