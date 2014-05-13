e06e3b43b6059c9ada4478e88f880b0ea817bb86
fabianofranz <contact@fabianofranz.com>
2014-05-13 18:51:24 -0300

    Testing

--- mods/com.fabianofranz~roland-deschain~1.0/Instagram.java
+++ mods/com.fabianofranz~roland-deschain~1.0/Instagram.java
@@ -10,21 +10,21 @@ public class Instagram {
   static private final String CALLBACK_URL = "http://jbossvertx-ffranz.rhcloud.com/instagram/event"; 
 
   static public void requestSubscriptionToGeography(Double latitude, Double longitude, Integer radius) {
-    String url = new StringBuilder().
-      append(INSTAGRAM_API_HOST).
-      append(INSTAGRAM_API_ENDPOINT).
-      append('?').
-      append("client_id=").append(Config.instagramClientId()).append('&').
-      append("client_secret=").append(Config.instagramClientSecret()).append('&').
-      append("object=geography").append('&').
-      append("aspect=media").append('&').
-      append("lat=").append(latitude.toString()).append('&').
-      append("lng=").append(longitude.toString()).append('&').
-      append("radius=").append(radius.toString()).append('&').
-      append("callback_url=").append(encode(CALLBACK_URL)).toString();
+    String url = new StringBuilder().append(INSTAGRAM_API_HOST).append(INSTAGRAM_API_ENDPOINT).toString();
+
     try {
       System.out.println("Going to POST: " + url);
-      String response = Request.Post(url).execute().returnContent().asString();
+      String response = Request.Post(url).
+        bodyForm(Form.form().
+          add("client_id", Config.instagramClientId()).
+          add("client_secret", Config.instagramClientSecret()).
+          add("object", "geography").
+          add("aspect", "media").
+          add("lat", latitude.toString()).
+          add("lng", longitude.toString()).
+          add("radius", radius.toString()).
+          add("callback_url", encode(CALLBACK_URL)).build()).
+        execute().returnContent().asString();
       System.out.println("requestSubscribeToGeography returned: " + response);
     } catch (Exception e) {
       e.printStackTrace();
