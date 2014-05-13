d0d1f956166393c344fb1fb5502bacfc92a2645b
fabianofranz <contact@fabianofranz.com>
2014-05-13 13:22:50 -0300

    Testing

--- Config.java
+++ Config.java
@@ -1,5 +1,13 @@
 public class Config {
 
+  static public String serverIp() {
+    return get("OPENSHIFT_VERTX_IP", "127.0.0.1");
+  }
+
+  static public Integer serverPort() {
+    return getInteger("OPENSHIFT_VERTX_PORT", 8080);
+  }
+
   static public String get(String envVarName) {
     return System.getenv(envVarName);
   }
@@ -16,14 +24,6 @@ public class Config {
   static public Integer getInteger(String envVarName, Integer defaultValue) {
     Integer value = getInteger(envVarName);
     return value == null ? defaultValue : value;
-  }
-
-  static public String serverIp() {
-    return get("OPENSHIFT_VERTX_IP", "127.0.0.1");
-  }
-
-  static public Integer serverPort() {
-    return getInteger("OPENSHIFT_VERTX_PORT", "8080");
   }
 
 }
