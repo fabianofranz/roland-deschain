7b2dc5f71090f3d4f1212a3021777791e98bc7d5
fabianofranz <contact@fabianofranz.com>
2014-05-15 00:14:10 -0300

    foo

---
+++ mods/com.fabianofranz~roland-deschain~1.0/Cache.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Cache {
+
+  private 
+
+  static private final Cache INSTANCE = new Cache();
+
+  static public Cache get() {
+    return INSTANCE;
+  }
+
+  private Cache() { }
+
+  public List<String> event(String json) {
+    List<String> details = new ArrayList<String>();
+    Set<String> geographies = Instagram.wasNotifiedFor(json);
+    for (String geography : geographies) {
+      details.add(Instagram.fetchGeographyDetails(geography));
+    }
+    return details;
+  }
+
+}
