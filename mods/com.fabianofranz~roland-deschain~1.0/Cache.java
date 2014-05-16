6a2459e5f9c156e973c3b78947c9bddd374fa89e
fabianofranz <contact@fabianofranz.com>
2014-05-16 18:52:04 -0300

    foo

--- mods/com.fabianofranz~roland-deschain~1.0/Cache.java
+++ mods/com.fabianofranz~roland-deschain~1.0/Cache.java
@@ -6,17 +6,20 @@ import java.util.LinkedHashSet;
 import java.util.Collections;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+import java.util.Queue;
 
 public class Cache {
 
+  static private final Integer CACHE_MAX_SIZE = 300;
   static private final Cache INSTANCE = new Cache();
-  private final Set<JsonObject> cache = Collections.synchronizedSet(new LinkedHashSet<JsonObject>());
+  private final Queue<JsonObject> cache = new CircularFifoQueue<JsonObject>(CACHE_MAX_SIZE);
 
   static public Cache get() {
     return INSTANCE;
   }
 
-  private Cache() { }
+  private Cache() {}
 
   public List<JsonObject> event(final String json) {
     return new ArrayList<JsonObject>() {{
