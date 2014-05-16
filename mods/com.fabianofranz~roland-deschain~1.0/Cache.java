84ae36e9c874cf73b9cf585a9d966a4ef36fc724
fabianofranz <contact@fabianofranz.com>
2014-05-16 18:37:17 -0300

    foo

--- mods/com.fabianofranz~roland-deschain~1.0/Cache.java
+++ mods/com.fabianofranz~roland-deschain~1.0/Cache.java
@@ -2,12 +2,15 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Map;
+import java.util.LinkedHashSet;
+import java.util.Collections;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 
 public class Cache {
 
   static private final Cache INSTANCE = new Cache();
+  private final Set<JsonObject> cache = Collections.synchronizedSet(new LinkedHashSet<JsonObject>());
 
   static public Cache get() {
     return INSTANCE;
@@ -18,7 +21,9 @@ public class Cache {
   public List<JsonObject> event(final String json) {
     return new ArrayList<JsonObject>() {{
       for (String id : Instagram.parseEventObjectIds(json)) {
-        add(Instagram.fetchGeographyDetails(id));
+        JsonObject details = Instagram.fetchGeographyDetails(id);
+        cache.add(details);
+        add(details);
       }
     }};
   }
